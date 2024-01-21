7cb36fb05fb5fa1b1c6d69161080db993e7234e7
isusudmax122 <rachedbenhssouna@gmail.com>
2024-01-21 17:41:39 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import MOviesList from './components/MOviesList';
+import NavBar from './components/NavBar';
+import React, { useState } from 'react';
+
+
+function App() {
+    const [search, setSearch] = useState("");
+   const [rate, setRate] = useState(0);
+   
+  return (
+    <div className="App">
+     <NavBar setSearch={setSearch} setRate={setRate} rating={rate}/>
+     <MOviesList rate={rate} search={search} />
+    </div>
+  );
+}
+
+export default App;
